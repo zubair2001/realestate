1e655e8621e3de8d7369eb6363ff48250d7df32c
zubair2001 <zubair_mansuri2001@outlook.com>
2022-10-20 23:05:15 +0500

    DAY 3

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,17 +1,24 @@
-import Head from 'next/head'
-import {Box} from '@chakra-ui/react'
+import Head from 'next/head';
+import { Box } from '@chakra-ui/react';
 
-const Layout=()=>(
-    <>
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+export default function Layout({ children }) {
+    return (
+        <>
         <Head>
             <title>Real Estate</title>
-            <Box maxWidth="1200px" m="auto">
-                <header>Navbar</header>
-                <main>{children}</main>
-                <footer>Footer</footer>
-            </Box>
         </Head>
-    </>
-);
-
-export default Layout;
+        <Box maxWidth='1280px' m='auto'>
+            <header>
+            <Navbar />
+            </header>
+            <main>{children}</main>
+            <footer>
+            <Footer />
+            </footer>
+        </Box>
+        </>
+    );
+}
